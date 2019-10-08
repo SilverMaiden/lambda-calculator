726aef40c94babf94c40b5c976501ad129571715
SilverMaiden <h.specials@gmail.com>
2019-10-08 10:17:19 -0700

    added working functionality for C button

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -6,11 +6,9 @@ const SpecialButton = (props) => {
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       <button onClick={() => {
 
-          console.log(props.value);
           if (props.value === 'C') {
-              props.setNumberState(props.numberState - props.numberState);
+              props.setNumberState(parseInt(props.numberState) - parseInt(props.numberState));
           }
-          console.log(props.numberState);
 
 
       }}> {props.value} </button>
