2c42aa4757e60a282b7893ab96087733926a7e6e
SilverMaiden <h.specials@gmail.com>
2019-10-08 14:20:12 -0700

    working calculator functionality, need to work on styling

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -6,33 +6,9 @@ const OperatorButton = (props) => {
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       <button onClick={() => {
-          console.log(props.value);
           if (props.value === '=') {
-              console.log("TIME TO FIND THE SUM");
-              let myArr = Array.from(props.numberState);
-              let temporaryTotal = "";
-              let finalOutput = 0;
-              myArr.forEach((element) => {
-                  console.log(element);
-                  let strInt = parseInt(element);
-                  if (isNaN(strInt)) {
-                      let intTotal = parseInt(temporaryTotal);
-                      if (element === '+') {
-                          finalOutput += intTotal ;
-                          console.log(finalOutput);
-                      } else if (element === '-') {
-                          finalOutput -= intTotal;
-                      } else if (element === '*') {
-                          finalOutput = finalOutput * intTotal;
-                      } else if (element === '/') {
-                          finalOutput = finalOutput / intTotal;
-                      }
-                      temporaryTotal = "";
-                  } else {
-                      temporaryTotal += element;
-                  }
-              })
-              console.log(finalOutput);
+              let myStr = props.numberState.replace("x", "*");
+              props.setNumberState(eval(props.numberState.replace("x", "*")));
           } else {
               props.setNumberState(props.numberState + props.value);
           }
